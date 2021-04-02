ac7fa8d9e894684ebba054436f66f5c5bb6ac66d
pkwok123 <peiyikwok@gmail.com>
2021-04-02 10:28:10 -1000

    Add missing files from previous commit

---
+++ src/components/ContentIsbn.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import { Chip, Grid, withStyles } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+
+const styles = {
+  outlinedPrimary: {
+    color: "white",
+    border: "1px solid #ffffff",
+  },
+  clickableColorPrimary: {
+    color: "white",
+    backgroundColor: "#6b6b6b",
+    border: "1px solid #6b6b6b",
+  },
+  deleteIconOutlinedColorPrimary: {
+    color: "white",
+  },
+};
+
+const ContentIsbn = ({ number, handleChipOnDelete, classes }) => (
+  <Chip
+    label={number}
+    variant="outlined"
+    color="primary" //uses primary properties to modify css, not material ui primary color
+    clickable
+    deleteIcon={<AddIcon />}
+    onDelete={handleChipOnDelete}
+    classes={{
+      outlinedPrimary: classes.outlinedPrimary,
+      clickableColorPrimary: classes.clickableColorPrimary,
+      deleteIconOutlinedColorPrimary: classes.deleteIconOutlinedColorPrimary,
+    }}
+  />
+);
+
+export default withStyles(styles)(ContentIsbn);
---
+++ src/components/ContentMoreDetails.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import { Typography, withStyles } from "@material-ui/core";
+
+const styles = {
+  fontFTG: {
+    fontFamily: "Fredericka the Great",
+  },
+};
+
+const ContentMoreDetails = ({
+  series,
+  publishName,
+  publishYear,
+  children,
+  classes,
+}) => (
+  <Typography variant="body1" className={classes.fontFTG}>
+    {series !== "None" ? `Series: ${series}` : null}
+    {`Publisher: ${publishName}`}
+    {` (${publishYear})`}
+    <br />
+    {`Subject: `}
+    {children}
+  </Typography>
+);
+
+export default withStyles(styles)(ContentMoreDetails);

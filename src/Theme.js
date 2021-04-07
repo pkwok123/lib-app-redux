e6f0e825574b480574a4fd720a5c1497404e59a5
pkwok123 <peiyikwok@gmail.com>
2021-04-07 10:34:08 -1000

    Style adding responsive font-size to Typography component

--- src/Theme.js
+++ src/Theme.js
@@ -4,8 +4,16 @@ import red from "@material-ui/core/colors/red";
 
 const Theme = createMuiTheme({
   typography: {
+    h2: {
+      "@media (max-width:600px)": {
+        fontSize: "2.375rem",
+      },
+    },
     h6: {
       fontWeight: "bold",
+      "@media (max-width:600px)": {
+        fontSize: "1.125rem",
+      },
     },
     body2: {
       fontWeight: "bold",

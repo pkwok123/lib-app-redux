c43706e140d17cf26dd8a905eba315b5558726ab
pkwok123 <peiyikwok@gmail.com>
2021-04-08 13:50:52 -1000

    Refractor SearchBar component's state and logic to Header container

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,7 +10,6 @@ import Paper from "@material-ui/core/Paper";
 import SvgIcon from "@material-ui/core/SvgIcon";
 //import { mdiBarcodeScan } from "@mdi/js";
 import Button from "@material-ui/core/Button";
-import Filter from "./Filter";
 
 const useStyles = withStyles((theme) => ({
   grow: {
@@ -27,71 +26,43 @@ const useStyles = withStyles((theme) => ({
   },
 }));
 
-class SearchBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      query: "",
-    };
-    this.handleChangeQuery = this.handleChangeQuery.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+const SearchBar = ({
+  query,
+  handleChangeQuery,
+  handleSubmit,
+  classes,
+  children,
+}) => (
+  <Paper className={classes.paper} square>
+    <Grid container md={8} spacing={1} wrap="nowrap">
+      <Grid item className={classes.grow}>
+        <form onSubmit={handleSubmit}>
+          <FormControl fullWidth variant="outlined">
+            <OutlinedInput
+              placeholder="Search..."
+              value={query}
+              onChange={handleChangeQuery}
+              autoFocus
+              required
+              endAdornment={
+                <InputAdornment>
+                  <Button className={classes.barcodeBtn} color="primary">
+                    <SvgIcon viewBox="0,0,24,24">
+                      <path
+                        fill="currentColor"
+                        d="M4,6H6V18H4V6M7,6H8V18H7V6M9,6H12V18H9V6M13,6H14V18H13V6M16,6H18V18H16V6M19,6H20V18H19V6M2,4V8H0V4A2,2 0 0,1 2,2H6V4H2M22,2A2,2 0 0,1 24,4V8H22V4H18V2H22M2,16V20H6V22H2A2,2 0 0,1 0,20V16H2M22,20V16H24V20A2,2 0 0,1 22,22H18V20H22Z"
+                      />
+                    </SvgIcon>
+                  </Button>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+        </form>
+      </Grid>
+      <Grid item>{children}</Grid>
+    </Grid>
+  </Paper>
+);
 
-  componentDidMount() {
-    this.setState({ query: this.props.queryStr });
-  }
-
-  handleChangeQuery(event) {
-    this.setState({ query: event.target.value });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    this.props.history.push(`/search?q=${this.state.query}`);
-  }
-
-  render() {
-    const { classes } = this.props;
-    return (
-      <Paper className={classes.paper} square>
-        <Grid container md={8} spacing={1} wrap="nowrap">
-          <Grid item className={classes.grow}>
-            <form onSubmit={this.handleSubmit}>
-              <FormControl fullWidth variant="outlined">
-                <OutlinedInput
-                  placeholder="Search..."
-                  value={this.state.query}
-                  onChange={this.handleChangeQuery}
-                  autoFocus
-                  required
-                  // startAdornment={
-                  //   <InputAdornment position="start">
-                  //     <SearchRoundedIcon />
-                  //   </InputAdornment>
-                  // }
-                  endAdornment={
-                    <InputAdornment>
-                      <Button className={classes.barcodeBtn} color="primary">
-                        <SvgIcon viewBox="0,0,24,24">
-                          <path
-                            fill="currentColor"
-                            d="M4,6H6V18H4V6M7,6H8V18H7V6M9,6H12V18H9V6M13,6H14V18H13V6M16,6H18V18H16V6M19,6H20V18H19V6M2,4V8H0V4A2,2 0 0,1 2,2H6V4H2M22,2A2,2 0 0,1 24,4V8H22V4H18V2H22M2,16V20H6V22H2A2,2 0 0,1 0,20V16H2M22,20V16H24V20A2,2 0 0,1 22,22H18V20H22Z"
-                          />
-                        </SvgIcon>
-                      </Button>
-                    </InputAdornment>
-                  }
-                />
-              </FormControl>
-            </form>
-          </Grid>
-          <Grid item>
-            <Filter />
-          </Grid>
-        </Grid>
-      </Paper>
-    );
-  }
-}
-
-export default withRouter(useStyles(SearchBar));
+export default useStyles(SearchBar);
--- src/containers/Header.js
+++ src/containers/Header.js
@@ -5,17 +5,83 @@ import { withRouter } from "react-router-dom";
 import { searchReset } from "../actions";
 import Header from "../components/Header";
 import SearchBar from "../components/SearchBar";
+import Filter from "../components/Filter";
+import { Component } from "react";
 
-const HeaderContainer = ({ location, searchReset }) => {
-  const URL = location.pathname;
-  const params = new URLSearchParams(location.search);
+class HeaderContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: "",
+    };
+    this.handleChangeQuery = this.handleChangeQuery.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    const { URL, queryStr } = this.props;
+    if (URL === "/search") {
+      this.setState({ query: queryStr });
+    }
+  }
+
+  handleChangeQuery(event) {
+    this.setState({ query: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.history.push(`/search?q=${this.state.query}`);
+  }
+
+  render() {
+    const { URL, searchReset } = this.props;
+    const { query } = this.state;
+    return (
+      <React.Fragment>
+        <Header URL={URL} searchReset={searchReset} />
+        {URL === "/search" ? (
+          <SearchBar
+            query={query || ""}
+            handleChangeQuery={this.handleChangeQuery}
+            handleSubmit={this.handleSubmit}
+          >
+            <Filter />
+          </SearchBar>
+        ) : null}
+      </React.Fragment>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  // React Router does not parse query strings anymore
+  const URL = ownProps.location.pathname;
+  const params = new URLSearchParams(ownProps.location.search);
   const queryStr = params.get("q");
-  return (
-    <React.Fragment>
-      <Header URL={URL} searchReset={searchReset} />
-      {URL === "/search" ? <SearchBar queryStr={queryStr || ""} /> : null}
-    </React.Fragment>
-  );
+
+  return {
+    URL,
+    queryStr,
+  };
 };
 
-export default withRouter(connect(null, { searchReset })(HeaderContainer));
+export default withRouter(
+  connect(mapStateToProps, { searchReset })(HeaderContainer)
+);
+
+// const HeaderContainer = ({ location, searchReset }) => {
+//   const URL = location.pathname;
+//   const params = new URLSearchParams(location.search);
+//   const queryStr = params.get("q");
+//   return (
+//     <React.Fragment>
+//       <Header URL={URL} searchReset={searchReset} />
+//       {URL === "/search" ? (
+//         <SearchBar queryStr={queryStr || ""}>
+//           <Filter />
+//         </SearchBar>
+//       ) : null}
+//     </React.Fragment>
+//   );
+// };
